f8a85700c6cc1e8eb059ec68f516c59baaddc8ed
aka <aaronkaltman@gmail.com>
2024-01-11 00:18:59 -0500

    new home page coming along for sure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import HomeLogosSection from "@/components/home-logos-section";
+import HomeServicesSection from "@/components/home-services-section";
 import MainHeader from "@/components/main-header";
 import MainHero from "@/components/main-hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-zinc-50">
       <MainHeader />
       <MainHero />
       <HomeLogosSection />
+      <HomeServicesSection />
+      <section className="flackinjurylawresources py-10">
+        <h2>Flack Injury Law Resources</h2>
+        <p>
+          The Flack Injury Blog contains information about personal injury law
+          in Atlanta and the State of Georgia. We specialize in Truck Accidents,
+          Slip and Falls, Medical Transport, and other cases throughout the City
+          of Atlanta and state of Georgia.
+        </p>
+      </section>
     </main>
   );
 }
---
+++ src/components/home-services-section.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+export default function HomeServicesSection() {
+  return (
+    <section className="services">
+      <div className="max-w-6xl mx-auto">
+        <h2 className="text-center font-bold text-5xl pt-14 pb-4">
+          Georgia Personal Injury Law
+        </h2>
+        <p className="text-center pb-12 text-red-600 text-xl">
+          Flack Injury <span className="font-bold">Law Practice Areas</span>
+        </p>
+        <div className="grid md:grid-rows-2 md:grid-cols-3 gap-12 px-4">
+          <div className="space-y-3">
+            <h3 className="font-bold text-lg">
+              Tractor Trailer Wrecks, Trucking Accidents, and Commercial Vehicle
+              Collisions
+            </h3>
+            <p className="text-md">
+              If you’ve been injured by a commercial motor vehicle, you need an
+              attorney who understands the applicable federal regulations and
+              unique complexities of these cases. Attorney Jonathan Flack has
+              years of experience working on trucking cases, and he is ready to
+              begin fighting for you!
+            </p>
+          </div>
+          <div className="space-y-3">
+            <h3 className="font-bold text-xl">
+              Non-Emergency Medical Transport Collisions and Injuries
+            </h3>
+            <p>
+              If you’ve been injured in a non-emergency medical transport van,
+              Flack Injury Law is here to help. Whether the injury arose from
+              improper securing of a wheelchair, improper belting, or reckless
+              driving, we know how to maximize your compensation.
+            </p>
+          </div>
+          <div>
+            <h3 className="font-bold text-xl">
+              Inadequate Security, Premises Liability, and Crime Victims
+            </h3>
+            <p>
+              If you’ve been a victim of a crime at a business establishment,
+              you may be entitled to recover compensation from the property
+              owner through a claim of inadequate or negligent security. Give us
+              a call to discuss your case.
+            </p>
+          </div>
+          <div>
+            <h3 className="font-bold text-4xl">Hello 1</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+              beatae ratione officia eaque suscipit esse quas nemo soluta saepe
+              enim quibusdam eveniet eius, dolores odit dolore consequatur
+              deserunt et laborum, mollitia exercitationem dignissimos error,
+              reiciendis voluptas. Obcaecati minus laborum non. Voluptates hic
+              dolore eius harum, ipsum odit qui facere dolorum.
+            </p>
+          </div>
+          <div>
+            <h3 className="font-bold text-4xl">Hello 1</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+              beatae ratione officia eaque suscipit esse quas nemo soluta saepe
+              enim quibusdam eveniet eius, dolores odit dolore consequatur
+              deserunt et laborum, mollitia exercitationem dignissimos error,
+              reiciendis voluptas. Obcaecati minus laborum non. Voluptates hic
+              dolore eius harum, ipsum odit qui facere dolorum.
+            </p>
+          </div>
+          <div>
+            <h3 className="font-bold text-4xl">Hello 1</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+              beatae ratione officia eaque suscipit esse quas nemo soluta saepe
+              enim quibusdam eveniet eius, dolores odit dolore consequatur
+              deserunt et laborum, mollitia exercitationem dignissimos error,
+              reiciendis voluptas. Obcaecati minus laborum non. Voluptates hic
+              dolore eius harum, ipsum odit qui facere dolorum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
