aa5506f777afce7969ed25bda4ec01cf96ab83ef
aka <aaronkaltman@gmail.com>
2024-01-11 08:53:39 -0500

    logos a go

--- src/components/homepage/home-logos-section.tsx
+++ src/components/homepage/home-logos-section.tsx
@@ -10,23 +10,23 @@ export default function HomeLogosSection() {
             <Image
               src="https://wp.flackinjurylaw.com/wp-content/uploads/2024/01/state-bar-of-georgia.png"
               width={220}
-              height={200}
+              height={60}
               alt="Georgia Super Lawyer"
               className="rounded-md p-2 bg-white"
             />
             <Image
               src="https://wp.flackinjurylaw.com/wp-content/uploads/2024/01/unnamed.jpg"
               width={220}
-              height={200}
+              height={60}
               alt="Georgia Super Lawyer"
               className="rounded-md p-2 bg-white"
             />
             <Image
               src="https://wp.flackinjurylaw.com/wp-content/uploads/2024/01/gtla.png"
-              width={220}
-              height={200}
+              width={204}
+              height={60}
               alt="Georgia Super Lawyer"
-              className="rounded-md p-2 bg-white"
+              className="rounded-md p-2 bg-white h-[90px]"
             />
           </div>
         </div>
