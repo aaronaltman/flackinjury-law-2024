ae2a64a89c33be01c88881c06cbc3443a314a1b7
aka <aaronkaltman@gmail.com>
2024-01-12 18:26:36 -0500

    singleblogpostpage

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/main-footer";
+import MainHeader from "@/components/main-header";
 import Image from "next/image";
 import React from "react";
 
@@ -26,16 +28,28 @@ export default async function SinglePostPage({ params }: { params: any }) {
     .then((res) => res.json())
     .then((res) => res.data.postBy);
   return (
-    <div>
-      <h1>{singlePostData.title}</h1>
-      <Image
-        src={`${singlePostData.featuredImage.node.sourceUrl}`}
-        width={300}
-        height={200}
-        layout="responsive"
-        alt={singlePostData.title}
-      />
-      <div dangerouslySetInnerHTML={{ __html: singlePostData.content }} />
+    <div className="">
+      <div className="bg-zinc-50">
+        <MainHeader />
+        <h1 className="prose lg:prose-xl font-bold py-4 px-4 text-center md:text-left">
+          {singlePostData.title}
+        </h1>
+      </div>
+      <div className="flex flex-col bg-zinc-50 border border-black/10 shadow-md">
+        <Image
+          src={`${singlePostData.featuredImage.node.sourceUrl}`}
+          width={150}
+          height={200}
+          layout="responsive"
+          alt={singlePostData.title}
+          className="object-cover max-h-[300px] rounded-b-xl shadow-xl"
+        />
+        <div
+          className="prose prose-lg mx-auto py-10 px-4"
+          dangerouslySetInnerHTML={{ __html: singlePostData.content }}
+        />
+        <Footer />
+      </div>
     </div>
   );
 }
