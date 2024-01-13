76851966bc1d3ff7303bdbbbd929086236e24a8a
aka <aaronkaltman@gmail.com>
2024-01-12 19:20:53 -0500

    update

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,11 +1,11 @@
-import { Link } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function MainHeader() {
   return (
-    <header className="flex flex-col items-center py-8 px-4 md:flex-row md:justify-between bg-slate-900">
-      <a href="/">
+    <header className="z-0 flex flex-col items-center py-8 px-4 md:flex-row md:justify-between bg-slate-900 ">
+      <Link href="/">
         <Image
           src="https://wp.flackinjurylaw.com/wp-content/uploads/2023/12/flack-injury-law-logo-clearer-PNG.png"
           alt="flack injury law logo"
@@ -13,7 +13,7 @@ export default function MainHeader() {
           height={79}
           className="pb-6 md:pb-0"
         />
-      </a>
+      </Link>
       <nav>
         <ul className="flex flex-wrap justify-center items-center font-bold text-lg md:text-xl py-4 md:py-0 text-white gap-4">
           <li className="hidden md:block">Home</li>
