6391083e6ff8252459c80696073acbec306b0436
aka <aaronkaltman@gmail.com>
2024-01-12 20:53:53 -0500

    still got the practice areas and favicon andanime

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,15 @@
+import Footer from "@/components/main-footer";
+import MainHeader from "@/components/main-header";
 import React from "react";
 
 export default function BlogPage() {
-  return <div>Posts go Here</div>;
+  return (
+    <div className="flex flex-col max-w-6xl mx-auto">
+      <MainHeader />
+      <div className="h-[36dvh]">Posts go Here</div>
+      <div className="mt-auto">
+        <Footer />
+      </div>
+    </div>
+  );
 }
--- src/app/pages/[uri]/layout.tsx
+++ src/app/pages/[uri]/layout.tsx
@@ -5,5 +5,5 @@ export default function PageLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="max-w-6xl mx-auto">{children}</div>;
+  return <div className="max-w-6xl mx-auto bg-zinc-50">{children}</div>;
 }
