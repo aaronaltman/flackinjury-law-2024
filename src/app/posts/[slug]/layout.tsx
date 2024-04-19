ae267782b53ca8745ee671961574225b5699f8b3
aka <aaronkaltman@gmail.com>
2024-04-18 20:35:17 -0400

    updating meta for posts

--- src/app/posts/[slug]/layout.tsx
+++ src/app/posts/[slug]/layout.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Flack Injury Law | Atlanta, GA Personal Injury Attorney",
+  description:
+    "Flack Injury Law specializes in personal injury cases in Georgia. Get fast settlements and speak directly to your attorney. Expertise in auto accidents, slip and falls, and more.",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div className="max-w-6xl mx-auto">{children}</div>;
